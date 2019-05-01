47afb76452674fc5dbb564f7c7e299b787d67a25
Inayat Cassambai <inu3219@gmail.com>
2019-05-01 18:54:13 +0100

    added missing directory path

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function my_assets() {
 	wp_enqueue_script('jquery'); 
 
 	// Register Custom Javascript Files For Site
-    wp_register_script('Theme-script', get_stylesheet_directory_uri().'/assets/js/scripts.min.js', array(), "", true);
+    wp_register_script('Theme-script', get_stylesheet_directory_uri().'/assets/js/dist/scripts.min.js', array(), "", true);
 
     // Register Custom CSS For Site
     wp_register_style('Theme-stylesheet', get_stylesheet_directory_uri().'/style.css');
