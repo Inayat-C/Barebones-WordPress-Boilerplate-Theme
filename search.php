5322f8a7fc3e3b434eb59a44555811f3d7bded11
Inayat Cassambai <inu3219@gmail.com>
2019-05-01 17:22:13 +0100

    Updated default search template to be more useful

--- search.php
+++ search.php
@@ -2,12 +2,22 @@
 
 	<div class="container">
 		<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
+
+			<section>
+				<h1> <?php the_title(); ?> </h1>
+				<?php the_excerpt(); ?>
+				<button> <a href="<?php the_permalink(); ?>"> Read More </a> </button>
+			</section>
+			
 		<?php endwhile; else : ?>
-		<?php 
-			_e( 'Nothing Found'); 
-		 	_e( 'Sorry, nothing matched your search. Please try again.'); 
-			get_search_form(); 
-		endif; ?>
+
+			<section>
+				<h1> <?php _e( 'Nothing Found'); ?> </h1>
+				<p> <?php _e( 'Sorry, nothing matched your search. Please try again.'); ?> </p>
+				<?php get_search_form(); ?>
+			</section>
+
+		<?php endif; ?>
 	</div>
 
 <?php get_footer(); ?>
