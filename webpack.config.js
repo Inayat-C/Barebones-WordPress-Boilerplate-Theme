d9a8b4c30110a008b5d59640194512a3c3766c1d
Inayat Cassambai <inu3219@gmail.com>
2020-02-06 15:45:19 +0000

    Added sourceMap options to each loaders to correctly output sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,70 @@ const   path = require('path'),
         UglifyJSPlugin = require('uglifyjs-webpack-plugin'),
         OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-    entry: './assets/js/index.js',
-    mode: 'development',
-    devtool: 'source-map',
-    output: {
-        filename: 'scripts.min.js',
-        path: path.resolve(`${__dirname}/assets/js/`, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    "css-loader?url=false",
-                    "postcss-loader", 
-                    "sass-loader"
-                ]
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+        console.log(process.env);
+  
+module.exports = env => {
+    return {
+        entry: './assets/js/index.js',
+        mode: 'development', 
+        devtool: 'source-map',
+        output: {
+            filename: 'scripts.min.js',
+            path: path.resolve(`${__dirname}/assets/js/`, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader, 
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                plugins: [
+                                    require('autoprefixer')
+                                ],
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                        
+                    ]
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
                     }
-                }
-            }         
-        ]
-    },
-    plugins: [ 
-        new MiniCssExtractPlugin({
-            filename: '../../../style.css',
+                }         
+            ]
+        },
+        plugins: [ 
+            new MiniCssExtractPlugin({
+                filename: '../../../style.css',
+            }
+        )],
+        optimization: {
+            minimizer: [
+                new UglifyJSPlugin(),
+                new OptimizeCssAssetsPlugin()
+            ]
         }
-    )],
-    optimization: {
-        minimizer: [
-            new UglifyJSPlugin(),
-            new OptimizeCssAssetsPlugin()
-        ]
     }
+
 };
